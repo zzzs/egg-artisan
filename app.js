ae1ef2f5445e13ec0fc2bed42a7a648f2e4cc388
zhangxianlei <863837949@qq.com>
2018-09-17 23:22:11 +0800

    lazy commit

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const path = require('path');
 
 module.exports = app => {
   app.runArtisan = async (artisanPath, argvs) => {
+    let rawArgv = artisanPath.split(' ');
+    rawArgv = rawArgv.filter(item => item !== '');
+
+    rawArgv[0] = path.basename(rawArgv[0]);
+
+    artisanPath = rawArgv[0];
     if (!path.isAbsolute(artisanPath)) {
       artisanPath = path.join(app.config.baseDir, 'app/artisan', artisanPath);
     }
     let artisanClass = require.resolve(artisanPath);
     artisanClass = require(artisanClass);
-    let rawArgv = artisanPath.split(' ');
-    rawArgv[0] = path.basename(rawArgv[0]);
     if (argvs) {
       if (is.array(argvs)) {
         array.forEach(item => {
@@ -26,8 +30,7 @@ module.exports = app => {
         throw new Error('argvs error');
       }
     }
-
-    const artisanObj = new artisanClass(argvs);
+    const artisanObj = new artisanClass(rawArgv);
     artisanObj.ctx = app.createAnonymousContext();
     await artisanObj.run(artisanObj.context);
   };
