c10b1d7b51ea97f18e81bfc82362827dad8bbf96
zhangxianlei <863837949@qq.com>
2018-09-17 08:21:27 +0800

    add app.runArtisan

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 'use strict';
+
+const is = require('is-type-of');
+const path = require('path');
+
 module.exports = app => {
+  app.runArtisan = async (artisanPath, argvs) => {
+    if (!path.isAbsolute(artisanPath)) {
+      artisanPath = path.join(app.config.baseDir, 'app/artisan', artisanPath);
+    }
+    const artisanClass = require.resolve(artisanPath);
+    artisanClass = require(artisanClass);
+    console.log(artisanClass)
+    let rawArgv = artisanPath.split(' ');
+    rawArgv[0] = path.basename(rawArgv[0]);
+    if (argvs) {
+      if (is.array(argvs)) {
+        array.forEach(item => {
+          rawArgv.push(item);
+        });
+      } else if (is.object(argvs)) {
+        for (let item in argvs) {
+          rawArgv.push(item);
+          rawArgv.push(argvs[item]);
+        }
+      } else {
+        throw new Error('argvs error');
+      }
+    }
 
+    const artisanObj = new artisanClass(argvs);
+    artisanObj.ctx = app.ctx;
+    await artisanObj.run(artisanObj.context);
+  };
 };
-
